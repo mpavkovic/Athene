95f2019208d8f54e2ddacc8d3da19ace40b50816
Milan Pavkovic <pauk960@gmail.com>
2011-05-26 23:33:23 +0200

    Forgot to save changes.

--- js/Athene/controller/Menu.js
+++ js/Athene/controller/Menu.js
@@ -1,36 +1,3 @@
-var regWindows = [
-    'razredlist',
-    'skgodlist',
-    'uceniklist',
-    'uciteljlist',
-	'drzavalist',
-	'mjestolist',
-	'nacionalnamanjinalist',
-	'nastavniplanlist',
-	'dogadjajlist',
-	'narodnostlist',
-	'skolalist',
-	'predmetlist',
-	'predmetrazrednogodjeljenjalist',
-	'poteskocalist',
-	'vrstaputnikalist',
-	'zanimanjalist',
-	'titulalist',
-	'kategorijaocjenalist',
-	'slobodnaaktivnostlist',
-	'razlogboravistalist',
-	'predmetnacjelinalist',
-	'ocjenalist',
-	'dodijeljenapoteskocalist',
-	'nastavnajedinicalist',
-	'metodskajedinicalist',
-	'nastavnisatlist',
-	'najavaispitalist',
-	'adresalist',
-	'izostanaklist',
-	'userlist'
-];
-
 Ext.define('Athene.controller.Menu', {
     extend: 'Ext.app.Controller',
     
@@ -46,10 +13,7 @@ Ext.define('Athene.controller.Menu', {
                     if(record.data.leaf == true) {
                         _id = record.data.id;
                         _win = _id.substring(_id.lastIndexOf("/")+1);
-                        /*if(!Ext.Array.contains(regWindows, _win)) {
-                            Ext.Msg.alert("Error", "Unknown window id " + _win + ".");
-                            return;
-                        }*/
+			
                         var view = Ext.WindowManager.get(_win);
                         if(view === undefined) {
 			    if(Ext.ComponentManager.isRegistered(_win)) {
